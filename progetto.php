9276d9a3f2a74a5e61b94b7aab81179833a62bbb
aporcu <a.porcu@hotmail.it>
2015-01-24 18:56:41 +0100

    start

---
+++ progetto.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	<meta http-equiv = "content-type" content = "text/html; charset=utf-8" />
+	
+	
+
+    </head>
+
+    <body>
+  
+	<div class="form-input">    
+	<h2>Login</h2>
+	<br/>       
+	
+	<form method="post" action="login">
+	
+	<!-- campo del form per lo username  -->
+	<label for="nome">Nome</label>                       
+	<input name="nome" id="matricola" type="text"/>
+	<br/>
+
+	<!-- campo del form per la password -->
+	<label for="password">Password</label>
+	<input type="password" name="password" id="password"
+	<br/>
+
+	<button type="submit">Login</button>
+	
+
+
+
