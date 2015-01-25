37bd32682365368b625a995c180d3d275b53a93e
aporcu <a.porcu@hotmail.it>
2015-01-25 19:05:36 +0100

    s

--- Html/progetto.php
+++ Html/progetto.php
@@ -1,35 +1,46 @@
 <!DOCTYPE html>
 <html>
     <head>
-	<title>Progetto amm</title>
-	<meta http-equiv = "content-type" content = "text/html; charset=utf-8" />
-	<link rel="shortcut icon" type="image/x-icon" href="../Immagini/   .ico" />
-	<link href="../Css/stile.css" rel="stylesheet" type="text/css" media="screen" />
+	   	<title>Progetto amm</title>
+	   	<meta http-equiv = "content-type" content = "text/html; charset=utf-8" />
+	   	<link rel="shortcut icon" type="image/x-icon" href="../Immagini/   .ico" />
+	   	<link href="../Css/stile.css" rel="stylesheet" type="text/css" media="screen" />
 	
-
-    </head>
+	</head>
 
     <body>
-  
-	<div class="formlog">    
-	<h2>Login</h2>
-	<br/>       
+    
+	   	<h2>Login</h2>
+		<br/>       
 	
-	<form method="post" action="login">
+		<form method="post" action="login">
 	
-	<!-- campo del form per lo username  -->
-	<label for="nome">Nome Utente</label>                       
-	<input name="nome" id="nome" type="text"/>
-	<br/>
+			<!-- campo del form per lo username  -->
+			<label for="nome">Nome Utente</label>                       
+			<input name="nome" id="nome" type="text"/>
+			<br/>
 
-	<!-- campo del form per la password -->
-	<label for="password">Password</label>
-	<input type="password" name="password" id="password">
-	<br/>
-	</form>
+			<!-- campo del form per la password -->
+			<label for="password">Password</label>
+			<input type="password" name="password" id="password">
+			<br/>
 
-	<button type="submit">Login</button>
-	
+			<button type="submit">Login</button>
+		</form>
+
+	<!-- Footer -->
+	<div class:"w3">
+	<p>
+		<a href="http://validator.w3.org/check/referer" title="HTML Valido"> HTML validator </a>
+		
 
 
 
+
+
+
+
+
+
+	</body>
+</html>
