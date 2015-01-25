7b016f8d207c0b59f87132a28fa9b093f6a59291
aporcu <a.porcu@hotmail.it>
2015-01-25 18:47:38 +0100

    s

--- Html/progetto.php
+++ Html/progetto.php
@@ -3,8 +3,8 @@
     <head>
 	<title>Progetto amm</title>
 	<meta http-equiv = "content-type" content = "text/html; charset=utf-8" />
-	<link rel="shortcut icon" type="image/x-icon" href="Immagini/   .ico" />
-	<link href="Css/stile.css" rel="stylesheet" type="text/css" media="screen" />
+	<link rel="shortcut icon" type="image/x-icon" href="../Immagini/   .ico" />
+	<link href="../Css/stile.css" rel="stylesheet" type="text/css" media="screen" />
 	
 
     </head>
@@ -19,13 +19,14 @@
 	
 	<!-- campo del form per lo username  -->
 	<label for="nome">Nome Utente</label>                       
-	<input name="nome" id="matricola" type="text"/>
+	<input name="nome" id="nome" type="text"/>
 	<br/>
 
 	<!-- campo del form per la password -->
 	<label for="password">Password</label>
 	<input type="password" name="password" id="password">
 	<br/>
+	</form>
 
 	<button type="submit">Login</button>
 	
