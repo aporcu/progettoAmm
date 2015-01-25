c0bca6db2352f97da7785927e0aa5010b1477c20
aporcu <a.porcu@hotmail.it>
2015-01-25 19:10:40 +0100

    s

--- Html/progetto.php
+++ Html/progetto.php
@@ -31,9 +31,11 @@
 	<!-- Footer -->
 	
 	<p>
-		<a href="http://validator.w3.org/check/referer" title="HTML Valido"> HTML validator </a>
+		<a href="http://validator.w3.org/check/referer" title="HTML Valido" target="_blank"> HTML Validator </a>
 
+		 <a href="http://jigsaw.w3.org/css-validator/check/referer" title="CSS Valido" target="_blank"> CSS Validator </a>
 
+	</p>
 
 
 
