e19cb372b60269717581329cf527adda7dc97797
aporcu <a.porcu@hotmail.it>
2015-01-26 19:01:40 +0100

    s

--- Html/primi.php
+++ Html/primi.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-	   	<title>Cucina</title>
+	   	<title>Cucina - Primi</title>
 	   	<meta http-equiv = "content-type" content = "text/html; charset=utf-8" />
 	   	<link rel="shortcut icon" type="image/x-icon" href="../Immagini/icon.ico" />
 	   	<link href="../Css/stile.css" rel="stylesheet" type="text/css" media="screen" />
@@ -25,29 +25,7 @@
 				<li><a href="../Html/desserts.php">Desserts</li>
 			</ul>
 
-	   	
-
-
-
-	   	<h2>Login</h2>
-		<br/>       
-	
-		<form method="post" action="login">
-	
-			<!-- campo del form per lo username  -->
-			<label for="nome">Nome Utente</label>                       
-			<input name="nome" id="nome" type="text"/>
-			<br/>
-
-			<!-- campo del form per la password -->
-			<label for="password">Password</label>
-			<input type="password" name="password" id="password">
-			<br/>
-
-			<button type="submit">Login</button>
-		</form>
-
-	
+	   
 	<!-- Footer -->
 		<p>
 			<a href="http://validator.w3.org/check/referer" title="HTML Valido" target="_blank"> HTML Validator </a>
--- Html/secondi.php
+++ Html/secondi.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-	   	<title>Cucina</title>
+	   	<title>Cucina - Secondi</title>
 	   	<meta http-equiv = "content-type" content = "text/html; charset=utf-8" />
 	   	<link rel="shortcut icon" type="image/x-icon" href="../Immagini/icon.ico" />
 	   	<link href="../Css/stile.css" rel="stylesheet" type="text/css" media="screen" />
@@ -26,27 +26,6 @@
 			</ul>
 
 	   	
-
-
-
-	   	<h2>Login</h2>
-		<br/>       
-	
-		<form method="post" action="login">
-	
-			<!-- campo del form per lo username  -->
-			<label for="nome">Nome Utente</label>                       
-			<input name="nome" id="nome" type="text"/>
-			<br/>
-
-			<!-- campo del form per la password -->
-			<label for="password">Password</label>
-			<input type="password" name="password" id="password">
-			<br/>
-
-			<button type="submit">Login</button>
-		</form>
-
 	
 	<!-- Footer -->
 		<p>
