d94613f067d15cd1558ef866f65d62b802c186cd
aporcu <a.porcu@hotmail.it>
2015-01-28 22:57:16 +0100

    s

--- Html/index.php
+++ Html/index.php
@@ -37,7 +37,6 @@
 	   			<h2>Login</h2>
 				<br/>       
 	
-			<div id="content">
 				<form method="post" action="login">
 	
 				<!-- campo del form per lo username  -->
@@ -54,17 +53,15 @@
 				</form>
 			</div>
 
-			</div>
+		</div>
 
 		<!-- Footer -->
-		<footer>
 		<div id="footer">
 			<p>
 				<a href="http://validator.w3.org/check/referer" title="HTML Valido" target="_blank"> HTML Validator </a>   <!-- Validatore Html -->
 				<a href="http://jigsaw.w3.org/css-validator/check/referer" title="CSS Valido" target="_blank"> CSS Validator </a>  <!-- Validatore Css -->
 			</p>
 		</div>
-		</footer>
 
 	</body>
 </html>
