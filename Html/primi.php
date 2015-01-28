4be7e435c9c43dd5526edf3bc28020a2d08d7811
aporcu <a.porcu@hotmail.it>
2015-01-28 17:46:48 +0100

    s

--- Html/primi.php
+++ Html/primi.php
@@ -28,11 +28,12 @@
 		</div>
 	   
 	<!-- Footer -->
+		<footer>
 		<p>
 			<a href="http://validator.w3.org/check/referer" title="HTML Valido" target="_blank"> HTML Validator </a>
 			<a href="http://jigsaw.w3.org/css-validator/check/referer" title="CSS Valido" target="_blank"> CSS Validator </a>
 		</p>
-
+		</footer>
 
 
 
