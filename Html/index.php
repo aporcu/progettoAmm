bb7dbb71feac1a31a6ed365ab7e1a9931f076d46
aporcu <a.porcu@hotmail.it>
2015-01-28 23:23:16 +0100

    s

--- Html/index.php
+++ Html/index.php
@@ -13,7 +13,7 @@
     <!-- Header -->
   
     	<div id="header">
-    		<img id="logoimg" title="logo" alt="logo Cucina" src="../Immagini/logo.jpg" width="175" height="132"/>
+    		<img id="logoimg" title="logo" alt="logo Cucina" src="../Immagini/logo.jpg" width="175" height="132"/></br>
   			Cucina
     	</div>
     	
