f72db9d9b56e480a5ac8c99dd9e93f13124515d3
aporcu <a.porcu@hotmail.it>
2015-02-06 21:58:59 +0100

    s

--- Html/config.php
+++ Html/config.php
@@ -1,6 +1,6 @@
 <?php 
 	// contiene informazioni necessarie per la connessione al database
-	$dsn="mysql:host=spano.sc.unica.it 80;dbname:amm14_porcuAlessandro";
+	$dsn="mysql:host=192.167.155.72;dbname:amm14_porcuAlessandro";
 
 	$db_user="porcuAlessandro";
 	$db_psw="lontra186";
