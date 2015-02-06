5d4b8bdd8c0779433971e3ca798e7232c3b86b49
aporcu <a.porcu@hotmail.it>
2015-02-06 22:04:39 +0100

    s

--- Html/connessione.php
+++ Html/connessione.php
@@ -4,7 +4,7 @@
 	try {  													//provo la connessione al database in caso contrario catturo l'eccezione
 			$db = new PDO("$dsn","$db_user","$db_psw");
 			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  //imposta il lancio delle eccezioni
-
+			echo "ok";
 	} 
 	catch (Exception $e) {
 			echo 'Attenzione:'.$e->getMessage();
