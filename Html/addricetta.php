678dc7132a1d229a602fbfd2cd9d7e8f943b7efa
aporcu <a.porcu@hotmail.it>
2015-02-18 18:30:43 +0100

    s

--- Html/addricetta.php
+++ Html/addricetta.php
@@ -1,4 +1,28 @@
 <?php 
+	include ("../Html/connessione.php");
+
+	//inizializzo il prepared statement
+	$stmt = $mysqli->stmt_init();
+
+	$query = "INSERT INTO ricette (titolo, immagine, descrizione, idTipoRicetta) VALUES (:titolo, :immagine, :descrizione, :idTipoRicetta)";
+
+	//preparo la query
+	$stmt->prepare($query);
+
+	//collego i parametri
+	$stmt->bind_param(':titolo', $_POST['titolo']);
+	$stmt->bind_param(':immagine', $_POST['immagine']);
+	$stmt->bind_param(':descrizione', $_POST['descrizione']);
+	$stmt->bind_param(':idTipoRicetta', $_POST['idTipoRicetta']);
+
+
+	if ($stmt->execute()) {
+    	echo "ok";
+	}
+	else {
+		echo "zobbia";
+	}
+
 	$var=$_POST['titolo'];
 	echo $var;
 ?>
