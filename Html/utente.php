abe263d435c02b6c54f1ebfb81ff23419f95b258
aporcu <a.porcu@hotmail.it>
2015-02-19 20:26:44 +0100

    s

--- Html/utente.php
+++ Html/utente.php
@@ -28,19 +28,35 @@
 			?>
 
 			<div id="homeut">
-				
-				<img id="logoprimi" title="primi" alt="Miniatura Primi" src="../Immagini/primi.jpg" width="150" height="80"/>
-				<h3> Primi Piatti </h3>
-				qui sfkldjsdf dfkljsfdlkja asfadflkjasdfadf sadfjkasdkfjasdf 
+				<table>
+					<tr>
+						<td><img id="logoprimi" title="primi" alt="Miniatura Primi" src="../Immagini/primi.jpg" width="150" height="80"/></td>
+						<td><h3> Primi Piatti </h3></td>
+					</tr>
+						<td></td>
+						<td>qui sfkldjsdf dfkljsfdlkja asfadflkjasdfadf sadfjkasdkfjasdf</td>
+					</tr>
+				</table>
 
-				<img id="logosecondi" title="secondi" alt="Miniatura Secondi" src="../Immagini/secondi.jpg" width="150" height="80"/>
-				<h3> Secondi Piatti </h3>
-				qui sfkldjsdf dfkljsfdlkja asfadflkjasdfadf sadfjkasdkfjasdf 
+				<table>
+					<tr>
+						<td><img id="logosecondi" title="secondi" alt="Miniatura Secondi" src="../Immagini/secondi.jpg" width="150" height="80"/></td>
+						<td><h3> Secondi Piatti </h3></td>
+					</tr>
+						<td></td>
+						<td>qui sfkldjsdf dfkljsfdlkja asfadflkjasdfadf sadfjkasdkfjasdf</td>
+					</tr>
+				</table>
 
-				<img id="logodolci" title="dolci" alt="Miniatura Dolci" src="../Immagini/dolci.jpg" width="150" height="80"/>
-				<h3> Desserts </h3>
-				qui sfkldjsdf dfkljsfdlkja asfadflkjasdfadf sadfjkasdkfjasdf 
-
+				<table>
+					<tr>
+						<td><img id="logodolci" title="dolci" alt="Miniatura Dolci" src="../Immagini/dolci.jpg" width="150" height="80"/></td>
+						<td><h3> Desserts </h3></td>
+					</tr>
+						<td></td>
+						<td>qui sfkldjsdf dfkljsfdlkja asfadflkjasdfadf sadfjkasdkfjasdf</td>
+					</tr>
+				</table>
 			</div>
 		</div>
 
