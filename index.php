e1ebd01ca02fe2d607a58b3e6ccf71d0155a6052
aporcu <a.porcu@hotmail.it>
2015-02-19 17:49:50 +0100

    s

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>Info</title>
+    </head>
+
+    <body>
+    	
+    	<h1> Informazioni sul progetto </h1><br/>
+    	<h2> Descrizione </h2>
+    	<p>
+	    	Le funzionalità di base sono la visualizzazione di ricette divise in categoria per gli utenti, con la possibilità di aggiunta delle ultime da parte degli admin.
+    	</p>
+    	<p>
+    		Per ogni ricetta abbiamo le seguenti informazioni:
+    		<ul>
+    			<li> Titolo </li>
+    			<li> Categoria </li>
+    			<li> Immagine d'esempio </li>
+    			<li> Descrizione </li>
+    		</ul>
+    </body>
