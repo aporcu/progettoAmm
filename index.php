30d9e89131300d6a0a6f51910825fb341bb7ded2
aporcu <a.porcu@hotmail.it>
2015-02-19 17:59:58 +0100

    s

--- index.php
+++ index.php
@@ -7,10 +7,10 @@
 
     <body>
     	
-    	<h1> Informazioni sul progetto </h1><br/>
+    	<h1> Informazioni sul progetto </h1>    	
     	<h2> Descrizione </h2>
     	<p>
-	    	Le funzionalità di base sono la visualizzazione di ricette divise in categoria per gli utenti, con la possibilità di aggiunta delle ultime da parte degli admin.
+	    	Le funzionalità di base sono la visualizzazione di ricette divise in categorie per gli utenti, con la possibilità di aggiunta delle ultime da parte degli admin attraverso un apposito form.
     	</p>
     	<p>
     		Per ogni ricetta abbiamo le seguenti informazioni:
@@ -20,4 +20,24 @@
     			<li> Immagine d'esempio </li>
     			<li> Descrizione </li>
     		</ul>
+
+    	</p>
+
+    	<h2>Requisiti del progetto</h2>
+    	<p>
+    		<ul>
+    			<li> Utilizzo di HTML e CSS</li>
+    			<li> Utilizzo di PHP e MySQL</li>
+    			<li>Due ruoli (utente semplice e admin)</li>
+    			<li>Funzionalità ajax</li>
+    		</ul>
+    	</p>
+  
+    	<h2>Accesso al progetto</h2>
+    	<p>
+    		La home page del progetto è: <a href="http://spano.sc.unica.it/amm2014/porcuAlessandro/Html/home.php">http://spano.sc.unica.it/amm2014/porcuAlessandro/Html/home.php</a>
+    	</p>
+    
+
+
     </body>
