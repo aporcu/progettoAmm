4575c33efdf559cd3ce4eaa72a370501672f7e3a
aporcu <a.porcu@hotmail.it>
2015-02-19 17:35:12 +0100

    s

--- Html/sidebar.php
+++ Html/sidebar.php
@@ -1,6 +1,6 @@
 <div id="sidebar">
 	<h2 class="sidetitle">Navigazione</h2>
 	<ul class="naviglist">
-		<li><a href="../Html/index.php">Home </a></li>
+		<li><a href="../Html/home.php">Home </a></li>
 	</ul>
 </div>
